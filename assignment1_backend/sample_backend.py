8efe6dfe10721868a3fd5e6f0174297378e1a4f0
Yogi Sunkara <sunkara.yogitha@gmail.com>
2020-04-13 17:50:55 -0700

    added a post method

--- assignment1_backend/sample_backend.py
+++ assignment1_backend/sample_backend.py
@@ -1,20 +1,30 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/users/<id>')
-def get_user(id):
-   if id :
-      for user in users['users_list']:
-        if user['id'] == id:
-           return user
-      return ({})
-   return users
-
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+   if request.method == 'GET':
+      search_username = request.args.get('name')
+      if search_username :
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username:
+               subdict['users_list'].append(user)
+         return subdict
+      return users
+   elif request.method == 'POST':
+      userToAdd = request.get_json()
+      users['users_list'].append(userToAdd)
+      resp = jsonify(success=True)
+      #resp.status_code = 200 #optionally, you can always set a response code.
+      # 200 is the default code for a normal response
+      return resp
 users = {
    'users_list' :
    [
