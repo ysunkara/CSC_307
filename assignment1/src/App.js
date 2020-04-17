35af7a6ed61d564f34e25a88dba7ec8d15c94314
Yogi Sunkara <sunkara.yogitha@gmail.com>
2020-04-16 21:00:53 -0700

    fixed all the issues with running things

--- assignment1/src/App.js
+++ assignment1/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Table from './Table'
 import Form from './Form'
+import axios from 'axios';
 
 class App extends Component {
 state = {
@@ -14,6 +15,18 @@ state = {
         return i != index
      }),
     })
+  }
+
+  componentDidMount() {
+     axios.get('http://localhost:5000/users')
+      .then(res => {
+        const characters = res.data.users_list;
+        this.setState({ characters });
+      })
+      .catch(function (error) {
+        //Not handling the error. Just logging into the console.
+        console.log(error);
+      });
   }
 
   handleSubmit = character => {
